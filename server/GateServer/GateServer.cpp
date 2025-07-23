6cfaae022eefe334133e7ab1ebf74e54c9694147
KKKK1723 <2052436429@example.com>
2025-07-23 15:47:38 +0800

    取消port 用gate_port

--- server/GateServer/GateServer.cpp
+++ server/GateServer/GateServer.cpp
@@ -12,8 +12,6 @@ int main()
     unsigned short gate_port = atoi(gate_port_str.c_str());
     try
     {
-
-        unsigned short port = static_cast<unsigned short>(8080);
         net::io_context ioc;
         boost::asio::signal_set signals(ioc, SIGINT, SIGTERM);
         signals.async_wait([&ioc](boost::system::error_code e, int signal_number) {
@@ -24,7 +22,7 @@ int main()
             ioc.stop();
             });
         std::cout<<"新开始"<<std::endl;
-        auto p = std::make_shared<CServer>(ioc, port);
+        auto p = std::make_shared<CServer>(ioc, gate_port);
         p->Start();
         ioc.run();
     }
