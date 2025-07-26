74ba4f31bfcdb27f11ec5a918378f3d5cdbcffdf
KKKK1723 <2052436429@example.com>
2025-07-26 20:39:56 +0800

    Update const.h

--- server/GateServer/const.h
+++ server/GateServer/const.h
@@ -2,23 +2,27 @@
 #include <boost/beast/http.hpp>
 #include <boost/beast.hpp>
 #include <boost/asio.hpp>
-#include<memory>
-#include<iostream>
-#include"Singleton.h"
-#include<map>
-#include<functional>
+#include <memory>
+#include <iostream>
+#include "Singleton.h"
+#include <map>
+#include <functional>
 #include <unordered_map>
-#include<json/json.h>
-#include<json/value.h>
-#include<json/reader.h>
-#include<boost/filesystem.hpp>
-#include<boost/property_tree/ptree.hpp>
-#include<boost/property_tree/ini_parser.hpp>
+#include <json/json.h>
+#include <json/value.h>
+#include <json/reader.h>
+#include <boost/filesystem.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <atomic>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
 
-namespace beast = boost::beast;         // from <boost/beast.hpp>
-namespace http = beast::http;           // from <boost/beast/http.hpp>
-namespace net = boost::asio;            // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+namespace beast = boost::beast;   // from <boost/beast.hpp>
+namespace http = beast::http;     // from <boost/beast/http.hpp>
+namespace net = boost::asio;      // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp; // from <boost/asio/ip/tcp.hpp>
 
 enum ErrorCodes
 {
@@ -26,7 +30,3 @@ enum ErrorCodes
     Error_Json = 1001,
     RPCFailed = 1002,
 };
-
-
-class ConfigMgr;
-extern ConfigMgr gCfgMgr;
