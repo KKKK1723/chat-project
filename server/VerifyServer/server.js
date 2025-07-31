1cf8eb9b26f53f27e1ebb1e98650601c33bdcec5
KKKK1723 <2052436429@example.com>
2025-07-31 22:10:23 +0800

    使用redis

--- server/VerifyServer/server.js
+++ server/VerifyServer/server.js
@@ -2,12 +2,29 @@ const grpc = require('@grpc/grpc-js')
 const message_proto = require('./proto')
 const const_module = require('./const')
 const { v4: uuidv4 } = require('uuid')
-const emailModule=require('./email')
+const emailModule = require('./email')
+const redis_module = require('./redis')
+
 
 async function GetVarifyCode(call, callback) {
     console.log("email is ", call.request.email)
-    try{
-        uniqueId = Math.floor(100000 + Math.random() * 900000).toString();
+    try {
+        let tmp_value = await redis_module.GetRedisValue(const_module.code_prefix + call.request.email);
+        let uniqueId = tmp_value;
+        if (tmp_value == null)
+        {
+            uniqueId = Math.floor(100000 + Math.random() * 900000).toString();//随机生成
+            let b = await redis_module.SetRedisKey(const_module.code_prefix + call.request.email, uniqueId, 180);
+            if (!b)
+            {
+                callback(null, {
+                    email: call.request.email,
+                   error:  const_module.Errors.RedisErr
+                });
+                return;
+            }
+        }
+
         console.log("uniqueId is ", uniqueId)
         let text_str =  '您的验证码为'+ uniqueId +'请三分钟内完成注册'
         //发送邮件
@@ -21,8 +38,7 @@ async function GetVarifyCode(call, callback) {
         let send_res = await emailModule.SendMail(mailOptions);
         console.log("send res is ", send_res)
 
-        callback(null, {
-            email: call.request.email,
+        callback(null, { email:  call.request.email,
             error:const_module.Errors.Success
         }); 
 
@@ -40,7 +56,7 @@ async function GetVarifyCode(call, callback) {
 function main() {
     var server = new grpc.Server()
     server.addService(message_proto.VarifyService.service, { GetVarifyCode: GetVarifyCode })
-    server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), () => {
+    server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
         server.start()
         console.log('grpc server started')        
     })
