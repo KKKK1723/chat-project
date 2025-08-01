c3013436805a3442c447ef17a1dd326997066d4a
KKKK1723 <2052436429@qq.com>
2025-08-01 17:05:24 +0800

    提供Get接口

--- server/GateServer/RedisMgr.cpp
+++ server/GateServer/RedisMgr.cpp
@@ -1,9 +1,8 @@
 #include "RedisMgr.h"
 
-
 RedisMgr::RedisMgr()
 {
-	auto& gCfgMgr = ConfigMgr::Inst();
+	auto &gCfgMgr = ConfigMgr::Inst();
 	auto host = gCfgMgr["Redis"]["Host"];
 	auto port = gCfgMgr["Redis"]["Port"];
 	auto pwd = gCfgMgr["Redis"]["PassWord"];
@@ -15,8 +14,19 @@ RedisMgr::~RedisMgr()
 	Close();
 }
 
-
 void RedisMgr::Close()
 {
 	_RedisConPool->Close();
+}
+
+std::string RedisMgr::Get(const std::string &key)
+{
+	auto redis_tmp = _RedisConPool->GetConnection();
+	auto value = redis_tmp->get(key);
+	_RedisConPool->ReturnConnection(move(redis_tmp));
+	if (value)
+	{
+		return value.value();
+	}
+	return "";
 }
--- server/GateServer/RedisMgr.h
+++ server/GateServer/RedisMgr.h
@@ -1,17 +1,18 @@
-#include"const.h"
-#include"RedisConPool.h"
-#include"Singleton.h"
-#include"ConfigMgr.h"
+#include "const.h"
+#include "RedisConPool.h"
+#include "Singleton.h"
+#include "ConfigMgr.h"
 
-class RedisMgr : public Singleton<RedisMgr>,public std::enable_shared_from_this<RedisMgr>
+class RedisMgr : public Singleton<RedisMgr>, public std::enable_shared_from_this<RedisMgr>
 {
 	friend class Singleton<RedisMgr>;
+
 public:
 	~RedisMgr();
 	void Close();
+	std::string Get(const std::string &key); // 接口
 private:
 	RedisMgr();
 	std::unique_ptr<sw::redis::Redis> _redis;
 	std::unique_ptr<RedisConPool> _RedisConPool;
 };
-
