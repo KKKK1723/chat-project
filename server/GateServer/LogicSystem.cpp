9036736424a71fb083148856559459ae3d5a8dcf
KKKK1723 <2052436429@qq.com>
2025-08-01 17:12:08 +0800

    实现注册请求

--- server/GateServer/LogicSystem.cpp
+++ server/GateServer/LogicSystem.cpp
@@ -1,6 +1,7 @@
 #include"LogicSystem.h"
 #include"HttpConnection.h"
 #include"VerifyGrpcClient.h"
+#include"RedisMgr.h"
 
 //注册Get请求
 void LogicSystem::RegGet(std::string url, HttpHandler handler)
@@ -71,6 +72,57 @@ LogicSystem::LogicSystem()
 			return true;
 
 		});
+
+	RegPost("/user_register", [](std::shared_ptr<HttpConnection> connection)
+			{
+
+				auto body_str = boost::beast::buffers_to_string(connection->_request.body().data());
+				std::cout << "receive body is " << body_str << std::endl;
+				connection->_response.set(http::field::content_type, "text/json");
+				Json::Value root;
+				Json::Value src_root;
+				Json::Reader reader;
+				bool success = reader.parse(body_str, src_root);
+				if (!success)
+				{
+					std::cout << "Failed to parse JSON data!" << std::endl;
+					root["error"] = ErrorCodes::Error_Json;
+					std::string json_str = root.toStyledString();
+					beast::ostream(connection->_response.body()) << json_str;
+					return true;
+				}
+
+				//判断验证码是否过期
+				std::string verify_code = RedisMgr::GetInstance()->Get(CODEPREFIX + src_root["email"].asString());
+				if (verify_code=="")
+				{
+					std::cout << " get varify code expired" << std::endl;
+					root["error"] = ErrorCodes::VerifyExpired;
+					std::string json_str = root.toStyledString();
+					beast::ostream(connection->_response.body()) << json_str;
+					return true;
+				}
+
+				//判断验证码是否错误 (用户可能输错了验证码)
+				if (verify_code != src_root["verifycode"].asString())
+				{
+					std::cout << " get varify code error , error is  verify_code != src_root[verifycode] " << std::endl;
+					root["error"] = ErrorCodes::VerifyExpired;
+					std::string json_str = root.toStyledString();
+					beast::ostream(connection->_response.body()) << json_str;
+					return true;
+				}
+
+
+				root["error"] = ErrorCodes::Success;
+				root["email"] = src_root["email"].asString();
+				root["user"] = src_root["user"].asString();
+				root["passwd"] = src_root["passwd"].asString();
+				root["confirm"] = src_root["confirm"].asString();
+				root["verifycode"] = src_root["verifycode"].asString();
+				std::string json_str = root.toStyledString();
+				beast::ostream(connection->_response.body()) << json_str;
+				return true; });
 }
 
 LogicSystem::~LogicSystem()
