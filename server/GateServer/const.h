ba0d4372af13bcac820d234d9ce4ba36281409ac
KKKK1723 <2052436429@qq.com>
2025-08-01 17:07:09 +0800

    更新配置

--- server/GateServer/const.h
+++ server/GateServer/const.h
@@ -1,33 +1,42 @@
 #pragma once
+#define CODEPREFIX "code_"
 #include <boost/beast/http.hpp>
 #include <boost/beast.hpp>
 #include <boost/asio.hpp>
-#include <memory>
-#include <iostream>
-#include "Singleton.h"
-#include <map>
-#include <functional>
+#include<memory>
+#include<iostream>
+#include"Singleton.h"
+#include<map>
+#include<functional>
 #include <unordered_map>
-#include <json/json.h>
-#include <json/value.h>
-#include <json/reader.h>
-#include <boost/filesystem.hpp>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/ini_parser.hpp>
-#include <atomic>
-#include <queue>
-#include <mutex>
-#include <condition_variable>
+#include<json/json.h>
+#include<json/value.h>
+#include<json/reader.h>
+#include<boost/filesystem.hpp>
+#include<boost/property_tree/ptree.hpp>
+#include<boost/property_tree/ini_parser.hpp>
+#include<atomic>
+#include<queue>
+#include<mutex>
+#include<condition_variable>
 #include <sw/redis++/redis++.h>
 
-namespace beast = boost::beast;   // from <boost/beast.hpp>
-namespace http = beast::http;     // from <boost/beast/http.hpp>
-namespace net = boost::asio;      // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp; // from <boost/asio/ip/tcp.hpp>
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
 enum ErrorCodes
 {
     Success = 0,
-    Error_Json = 1001,
-    RPCFailed = 1002,
+    Error_Json = 1001,//JSON解析失败
+    RPCFailed = 1002,//RPC请求错误
+    VerifyExpired=1003,//验证码过期
+    VerifyCodeErr = 1004,//验证码错误
+    UserExist=1005,//用户已存在
+    PasswdErr=1006,//密码错误
+    EmailNotMatch=1007,//邮箱不匹配
+    PasswdUpDateErr=1008,//密码更新失败
 };
+
+
