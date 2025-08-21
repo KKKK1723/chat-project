e27f6ebc723b35ad02b7144101fb83641c24bf42
KKKK1723 <2052436429@qq.com>
2025-08-21 21:35:57 +0800

    创建列表基类

---
+++ client/listitembase.h
@@ -0,0 +1,22 @@
+#ifndef LISTITEMBASE_H
+#define LISTITEMBASE_H
+#include "global.h"
+#include <QWidget>
+
+class ListItemBase : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ListItemBase(QWidget *parent = nullptr);
+    void SetItemType(ListItemType itemType);
+    ListItemType GetItemType();
+
+private:
+    ListItemType _itemType;
+
+public slots:
+
+signals:
+};
+
+#endif // LISTITEMBASE_H
