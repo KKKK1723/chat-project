99d869925de99060eb44934b86f3a99878a34027
KKKK1723 <2052436429@qq.com>
2025-08-21 21:36:27 +0800

    创建用户管理类

---
+++ client/usermgr.cpp
@@ -0,0 +1,24 @@
+#include "usermgr.h"
+
+void UserMgr::SetName(QString name)
+{
+    _name = name;
+}
+
+void UserMgr::SetUid(int uid)
+{
+    _uid = uid;
+}
+
+void UserMgr::SetToken(QString token)
+{
+    _token = token;
+}
+
+UserMgr::UserMgr()
+{
+}
+
+UserMgr::~UserMgr()
+{
+}
