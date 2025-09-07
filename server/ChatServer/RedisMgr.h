311937898de2b93661b4a007bfb27cbd994718b8
KKKK1723 <2052436429@qq.com>
2025-09-07 18:02:02 +0800

    删除无用代码

--- server/ChatServer/RedisMgr.h
+++ server/ChatServer/RedisMgr.h
@@ -4,19 +4,6 @@
 #include "ConfigMgr.h"
 #include"Defer.h"
 
-// class RedisMgr : public Singleton<RedisMgr>, public std::enable_shared_from_this<RedisMgr>
-// {
-// 	friend class Singleton<RedisMgr>;
-
-// public:
-// 	~RedisMgr();
-// 	void Close();
-// 	std::string Get(const std::string &key); // 接口
-// private:
-// 	RedisMgr();
-// 	std::unique_ptr<sw::redis::Redis> _redis;
-// 	std::unique_ptr<RedisConPool> _RedisConPool;
-// };
 
 class RedisMgr: public Singleton<RedisMgr>, 
 	public std::enable_shared_from_this<RedisMgr>
