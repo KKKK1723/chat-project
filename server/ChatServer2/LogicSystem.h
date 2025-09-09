9f8865a1b46dbfe6f1e2e476d63813c0cb0d090b
KKKK1723 <2052436429@qq.com>
2025-09-09 14:57:38 +0800

    实现用户查找

--- server/ChatServer2/LogicSystem.h
+++ server/ChatServer2/LogicSystem.h
@@ -1,20 +1,25 @@
 #pragma once
-#include"const.h"
-#include"Singleton.h"
-#include"LogicNode.h"
+#include "const.h"
+#include "Singleton.h"
+#include "LogicNode.h"
 
 struct UserInfo;
 class CSession;
-typedef  std::function<void(std::shared_ptr<CSession>, const short& msg_id, const std::string& msg_data)> FunCallBack;
-class LogicSystem:public Singleton<LogicSystem>
+typedef std::function<void(std::shared_ptr<CSession>, const short &msg_id, const std::string &msg_data)> FunCallBack;
+class LogicSystem : public Singleton<LogicSystem>
 {
-	friend class  Singleton<LogicSystem>;
+	friend class Singleton<LogicSystem>;
+
 public:
 	void PostMsgToQue(std::shared_ptr<LogicNode> msg);
 	void DealQueMsg();
 	void RegisterCallBacks();
-	void LoginHandler(std::shared_ptr<CSession> session, const short& msg_id, const std::string& msg_data);
+	void LoginHandler(std::shared_ptr<CSession> session, const short &msg_id, const std::string &msg_data);
+	void SearchInfo(std::shared_ptr<CSession>, const short &uid, const std::string &msg_data);
 	bool GetBaseInfo(std::string base_key, int uid, std::shared_ptr<UserInfo> &userinfo);
+	bool isPureDigit(const std::string tmp);
+	void GetUserByName(std::string name, Json::Value &rtvalue);
+	void GetUserByUid(std::string uid_str, Json::Value &rtvalue);
 	~LogicSystem();
 
 private:
@@ -26,4 +31,3 @@ private:
 	std::map<short, FunCallBack> _fun_callbacks;
 	std::atomic<bool> _b_stop;
 };
-
